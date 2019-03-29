87edd68eaec8c9094f7a580e8e1c53d615374bb4
Jake Webb <jakewebbsite@gmail.com>
2019-03-29 00:32:48 -0500

    some state of loading & trying to render strava logo

--- src/components/Map.js
+++ src/components/Map.js
@@ -9,47 +9,52 @@ import olFeature from 'ol/Feature'
 import olPoint from 'ol/geom/Point'
 import olStyle from 'ol/style/Style'
 import olIcon from 'ol/style/Icon'
+import { easeIn } from 'ol/easing'
 import OSM from 'ol/source/OSM'
 // import { BingMaps, Vector as olVectorSource } from 'ol/source'
 import { fromLonLat } from 'ol/proj'
+import debounce from 'lodash.debounce'
 import STRAVA_LOGO from '../images/strava-logo.svg'
 
 console.log(STRAVA_LOGO)
 
-const DENVER_LON_LAT = [-104.991531, 39.742043]
+const DENVER_COORD = fromLonLat([-104.991531, 39.742043])
 
 class Map extends React.Component {
   constructor () {
     super()
 
+
+    const baseLayer = new olTileLayer({
+      source: new OSM()
+    })
     const layer = new olVectorLayer({
       source: new olVectorSource()
     })
     const map = new olMap({
       view: new olView({
-        center: fromLonLat(DENVER_LON_LAT),
-        zoom: 9
+        center: DENVER_COORD,
+        zoom: 12
       }),
       layers: [
-        new olTileLayer({
-          source: new OSM()
-        }),
+        baseLayer,
         layer
       ],
       target: 'map'
     })
 
     this.state = {
+      baseLayer,
       layer,
       map
     }
   }
 
   componentDidMount () {
-    const { layer } = this.state
+    const { layer, map } = this.state
     const source = layer.getSource()
     const stravaLogoFeature = new olFeature({
-      geometry: new olPoint(fromLonLat(DENVER_LON_LAT))
+      geometry: new olPoint(DENVER_COORD)
     })
 
     stravaLogoFeature.setStyle(
@@ -59,10 +64,37 @@ class Map extends React.Component {
         })
       })
     )
+    // add strava logo after tiles load in
+    map.once('rendercomplete', () => {
+      source.addFeature(stravaLogoFeature)
+    })
+    // const tileLoadDebounce = debounce(() => {
+    //   source.addFeature(stravaLogoFeature)
+    //   map.un('postrender', tileLoadDebounce)
+    // }, 2000)
+    // map.on('postrender', tileLoadDebounce)
+    source.on('addfeature', () => {
+      console.log('feature added!')
+      stravaLogoFeature.getStyle().getImage().setScale(1)
+      map.getView().setZoom(12)
 
-    console.log(stravaLogoFeature)
+      setTimeout(this.zoomToExtent, 3000)
+      setTimeout(() => {
+        console.log(stravaLogoFeature.getStyle().getImage().setScale(.5))
+      }, 3000)
+    })
+  }
 
-    source.addFeature(stravaLogoFeature)
+  zoomToExtent = () => {
+    const { map } = this.state
+    const view = map.getView()
+
+    view.animate({
+      anchor: DENVER_COORD,
+      easing: easeIn,
+      duration: 1800,
+      zoom: 5
+    })
   }
 
   render () {
