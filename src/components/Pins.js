d1ae793138c9db8e58a6ec405c2c0d66aec41565
Jake Webb <jakewebbsite@gmail.com>
2019-04-02 00:35:27 -0500

    80 activities json

--- src/components/Pins.js
+++ src/components/Pins.js
@@ -1,19 +1,9 @@
 import React from  'react'
+import ACTIVITIES from '../data/activities'
 
 class Pins extends React.Component {
   componentDidMount () {
-    const clientId = '33983'
-    const url = `https://www.strava.com/oauth/authorize?client_id=33983&response_type=code&redirect_uri=http://localhost:6000&approval_prompt=force`
-    const config = {
-      // mode: 'no-cors'
-    }
-
-    fetch(url, config)
-      .then(res => res.json())
-      .then(res => {
-        console.log(res)
-      })
-      .catch(console.error)
+    console.log(ACTIVITIES.reverse())
   }
 
   render () {
